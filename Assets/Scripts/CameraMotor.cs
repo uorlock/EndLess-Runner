ee04fcee07ab484b4e66d4dd10c07872bd8add9b
davit paposhvili <davit paposhvili>
2017-05-21 21:30:10 +0400

    start project

---
+++ Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+
+    private float transition = 0.0f;
+    private float animationDuration = 2.0f;
+
+    private Vector3 startOffset;
+    private Vector3 moveVector;
+    private Vector3 animationOffset = new Vector3(0, 5, 5);
+
+    private Transform lookAt;
+
+
+    void Start()
+    {
+        lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+        startOffset = transform.position - lookAt.position;
+    }
+
+    void Update()
+    {
+        moveVector = lookAt.position + startOffset;
+
+        // X
+        moveVector.x = 0;
+
+        // Y
+        moveVector.y = Mathf.Clamp(moveVector.y, 3, 5);
+
+        if (transition > 1.0f)
+        {
+            transform.position = moveVector;
+        }
+        else
+        {
+            // Animation at the start of the game
+            transform.position = Vector3.Lerp(moveVector + animationOffset, moveVector, transition);
+            transition += Time.deltaTime * 1 / animationDuration;
+            transform.LookAt(lookAt.position + Vector3.up);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController contriller;
+    private Vector3 moveVector;
+
+    
+    private float animationDuration = 2.0f;
+    private float verticalVelocity = 0.0f;
+    private float gravity = 10.0f;
+    private float speed = 2.0f;
+
+    void Start()
+    {
+        contriller = GetComponent<CharacterController>();
+
+    }
+
+    void Update()
+    {
+        if (Time.time < animationDuration)
+        {
+            contriller.Move(Vector3.forward * speed * Time.deltaTime);
+            return;
+        }
+        moveVector = Vector3.zero;
+
+        if (contriller.isGrounded)
+        {
+            verticalVelocity = -0.5f;
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        // X - Left and Right
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        // Y - Up and Down
+        moveVector.y = verticalVelocity;
+
+        // Z - Forward and Backward
+        moveVector.z = speed;
+
+
+        contriller.Move(moveVector * speed * Time.deltaTime);
+    }
+
+}
